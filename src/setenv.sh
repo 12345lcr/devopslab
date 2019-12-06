e200dcaa81deef60d00a097124d9bb56cabc6ede
12345lcr <39396305+12345lcr@users.noreply.github.com>
2019-12-06 14:56:36 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,9 +1,9 @@
-export GITACCOUNT=
+export GITACCOUNT=12345lcr
 
-export MYCLUSTER=
-export KUBECONFIG=
+export MYCLUSTER=tektonknativebeijing14
+export KUBECONFIG=/tmp/ic/AA/.bluemix/plugins/container-service/clusters/tektonknativebeijing14/kube-config-tok02-tektonknativebeijing14.yml
 
 export REGISTRY=us.icr.io
-export NAMESPACE=
-export EMAIL=
-export INGRESS=
+export NAMESPACE=tektonknativebeijing14chenrui
+export EMAIL=463020546@qq.com
+export INGRESS=tektonknativebeijing14-0e3e0ef4c9c6d831e8aa6fe01f33bfc4-0001.jp-tok.containers.appdomain.cloud
